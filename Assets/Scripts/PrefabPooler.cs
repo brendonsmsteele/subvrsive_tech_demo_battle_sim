adfe7f8e29faf231e9c078234700ae3076c320e7
Brendon Steele <brendonsmsteele@gmail.com>
2025-02-28 13:21:32 -0600

    created a gameloop manager, initial controllers, and an object pooler.

---
+++ Assets/Scripts/PrefabPooler.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PrefabPooler : MonoBehaviour
+{
+    public GameObject prefab;  // The object to pool
+    public int initialPoolSize = 10;  // Starting pool size
+    public int expandAmount = 5;  // How many new objects to add when expanding
+
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
+    void Start()
+    {
+        // Initialize the pool
+        ExpandPool(initialPoolSize);
+    }
+
+    private void ExpandPool(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    public GameObject GetObject(Vector3 position, Quaternion rotation)
+    {
+        if (pool.Count == 0)
+        {
+            // Expand the pool when empty
+            ExpandPool(expandAmount);
+        }
+
+        GameObject obj = pool.Dequeue();
+        obj.transform.position = position;
+        obj.transform.rotation = rotation;
+        obj.SetActive(true);
+        return obj;
+    }
+
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
